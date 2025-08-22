c5e73a4536f329d75c4878e6642ccdd04647c383
Juan Oicata <jeotoicata@gmail.com>
2025-08-22 13:46:06 -0500

    primer commmit

---
+++ src/main/java/Proyecto/SafePlay/SafePlayApplication.java
@@ -0,0 +1,13 @@
+package Proyecto.SafePlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafePlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Proyecto/SafePlay/SafePlayApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto.SafePlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafePlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
